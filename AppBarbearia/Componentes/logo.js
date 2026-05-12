d2107cb6acce5ef88dc9585d6436b6b8197d784f
Guilherme Pereira de Vargas <guilhermepvargas79@gmail.com>
2026-05-12 17:03:37 -0300

    Tela Inicial

---
+++ AppBarbearia/Componentes/logo.js
@@ -0,0 +1,19 @@
+import { Image, StyleSheet } from 'react-native';
+
+export default function Logo() {
+  return (
+    <Image
+      source={require('../Imagens/logo.png')}
+      style={styles.logo}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 300,
+    height: 300,
+    marginTop: 40,
+    resizeMode: 'contain',
+  },
+});
